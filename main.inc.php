4e8a7c2f400e5ecd07bd7dfbe32d5655e6015546
Poen <poen0121@gmail.com>
2017-06-30 14:20:14 +0800

    Update file

--- main.inc.php
+++ main.inc.php
@@ -28,7 +28,7 @@ if (!class_exists('hpl_template')) {
 				$caller = end($caller);
 				$message = __CLASS__ . '::' . $caller['function'] . '(): ' . $message;
 			}
-			//echo message
+			//response message
 			$title = '';
 			switch ($errno) {
 				case E_PARSE :
